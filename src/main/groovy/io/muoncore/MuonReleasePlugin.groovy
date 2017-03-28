c5ee5eb95ee539fd05cf9a2deb54429dc247cd7a
David Dawson <david.dawson@simplicityitself.com>
2017-03-28 22:30:45 +0100

    First commit of release tools

---
+++ src/main/groovy/io/muoncore/MuonReleasePlugin.groovy
@@ -0,0 +1,106 @@
+package io.muoncore
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.publish.maven.MavenPublication
+
+class MuonReleasePlugin implements Plugin<Project> {
+  void apply(Project project) {
+
+    project.subprojects { subproject ->
+      if (project.exclude) {
+        def excludes = project.exclude.split(",").collect { ":$it"}
+        if (subproject.name in excludes) return
+      }
+      subproject.apply plugin: 'maven'
+      subproject.apply plugin: 'maven-publish'
+      subproject.apply plugin: "com.jfrog.artifactory"
+      subproject.apply plugin:'io.franzbecker.gradle-lombok'
+      subproject.apply plugin: 'groovy'
+
+      subproject.group = project.group
+      subproject.version = project.version
+
+      subproject.dependencies {
+        testCompile "org.codehaus.groovy:groovy-all:2.4.8"
+        testCompile "org.spockframework:spock-core:1.0-groovy-2.4"
+        testCompile 'cglib:cglib:2.2.2'
+        testCompile 'org.objenesis:objenesis:2.4'
+      }
+
+      subproject.compileJava {
+        sourceCompatibility = "1.8"
+        targetCompatibility = "1.8"
+      }
+
+      subproject.compileTestJava {
+        sourceCompatibility = "1.8"
+        targetCompatibility = "1.8"
+      }
+
+      subproject.repositories {
+        jcenter()
+      }
+
+      subproject.publishing {
+        publications {
+          mavenJava(MavenPublication) {
+            from components.java
+          }
+        }
+      }
+
+// ----------- Deployment --------------
+      subproject.artifactory {
+        contextUrl = 'https://simplicityitself.artifactoryonline.com/simplicityitself/'   //The base Artifactory URL if not overridden by the publisher/resolver
+        publish {
+          repository {
+            repoKey = 'muon'   //The Artifactory repository key to publish to
+            username = 'sergio'          //The publisher user name
+            password = 'cistechfutures'       //The publisher password
+          }
+          defaults {
+            publications ('mavenJava')
+            publishArtifacts = true
+            publishPom = true
+          }
+        }
+        resolve {
+          repository {
+            repoKey = 'muon'  //The Artifactory (preferably virtual) repository key to resolve from
+          }
+        }
+      }
+
+      if (project.plugins.hasPlugin('java')) {
+        // manifest.mainAttributes(provider: 'gradle')
+        subproject.configurations {
+          published
+        }
+
+
+        subproject.dependencies {
+          testCompile 'junit:junit:4.7'
+        }
+
+        subproject.task sourceJar(type: Jar) {
+          from sourceSets.main.allSource
+          classifier = 'sources'
+        }
+        subproject.task javadocJar(type: Jar, dependsOn: javadoc) {
+          classifier = 'javadoc'
+          from javadoc.destinationDir
+        }
+
+        // Add the sourceJars to non-extractor modules
+        subproject.artifacts {
+          published sourceJar
+          published javadocJar
+        }
+      }
+      configurations {
+        published
+      }
+    }
+  }
+}
